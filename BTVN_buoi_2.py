6f47e8fbd1e26e2453442c01579fb30b7663f153
khanhkhmt <153835059+khanhkhmt@users.noreply.github.com>
2024-07-20 21:30:10 +0700

    Add files via upload

---
+++ BTVN_buoi_2.py
@@ -0,0 +1,11 @@
+'''a  = float(input('nhập số thực a '))
+b =  float(input('nhập số thực b '))
+print ('tổng của 2 số thực là : ', '%.2f' % (a+b))'''
+
+
+'''s = input ('nhập 4 số :')
+a = s.split()
+x , y , z , t = map (int , a) 
+print (x + y + z + t)
+a , b , c = map (int , input('nhập 3 số nguyên : ').split())
+print (a+b+c)'''
